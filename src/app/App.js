20b0178812778667a433819ee30c685563dbdbb8
Eleanor Ruth <ellie.ruth@me.com>
2023-02-04 20:03:53 -0700

    rename files

--- src/app/App.js
+++ src/app/App.js
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from 'react';
 // import { Counter } from '../features/counter/Counter';
 import './App.css';
 import { getRestaurants } from "../apiCalls";
-import RestaurantMenuContainer from '../RestaurantMenuContainer/RestaurantMenuContainer.js';
+import RestaurantMenuContainer from '../RestaurantMenuContainer/RestaurantMenuContainer';
 
 
 const App = () => {
