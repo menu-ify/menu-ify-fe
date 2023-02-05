add2ac51a6be1bc4069f6f63a8159bb7fcbc8a33
Eleanor Ruth <ellie.ruth@me.com>
2023-02-05 10:35:20 -0700

    Test for ci

--- src/app/App.js
+++ src/app/App.js
@@ -2,21 +2,20 @@ import React, {useEffect, useState} from 'react';
 // import logo from '../../src/logo.svg'
 // import { Counter } from '../features/counter/Counter';
 import './App.css';
-import { getRestaurants } from "../apiCalls";
-import RestaurantMenuContainer from '../RestaurantMenuContainer/RestaurantMenuContainer';
+// import { getRestaurants } from "../apiCalls";
 
 
 const App = () => {
-  const [restaurants, setRestaurants] = useState([])
+  // const [restaurants, setRestaurants] = useState([])
 
-  useEffect(()=> {
-    getRestaurants().then(data => {
-      console.log("DATA", data.data)
-      setRestaurants(data.data)})
-  }, [])
+  // useEffect(()=> {
+  //   getRestaurants().then(data => {
+  //     console.log("DATA", data.data)
+  //     setRestaurants(data.data)})
+  // }, [])
   return (
     <div className="App">
-      <RestaurantMenuContainer restaurants={restaurants}/>
+      <h1>TEST</h1>
     </div>
   );
 }
