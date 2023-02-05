0c30749f39820556d232f31c698d38bc8f3ae26c
Eleanor Ruth <ellie.ruth@me.com>
2023-02-05 10:37:33 -0700

    Fix react linter error

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 // import logo from '../../src/logo.svg'
 // import { Counter } from '../features/counter/Counter';
 import './App.css';
