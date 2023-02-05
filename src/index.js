c64bd772b5a7e07e220133a437c09b67bbb568b0
Tim T <tim.email.personal@gmail.com>
2023-02-05 13:45:29 -1000

    change app to lower case

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 // import { Provider } from 'react-redux';
 // import { store } from './App/store';
-import App from './App/App'
+import App from './app/App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
