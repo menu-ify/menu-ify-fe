10e5446d01c01fc6d595f9ecdb95acb81d57933c
Tim T <tim.email.personal@gmail.com>
2023-02-05 12:57:48 -1000

    Add apicalls for post, delete, get

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,60 @@
-export const getRestaurants = () => {
-  return fetch('https://a1ecae6b-2320-4cd4-91ed-7da641c93480.mock.pstmn.io/api/v1/restaurants')
-    .then((res)=> res.json())
+// export const getRestaurants = () => {
+//   return fetch('https://a1ecae6b-2320-4cd4-91ed-7da641c93480.mock.pstmn.io/api/v1/restaurants')
+//     .then((res)=> res.json())
+// }
+
+function getData(url) {
+  return fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw Error(response.statusText)
+      }
+    })
+    .catch(error => {
+      console.log("Fetch error: ", error)
+    })
 }
+
+function deleteData(url) {
+  return fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw Error(response.statusText)
+      }
+    })
+    .catch(error => {
+      console.log("Fetch error: ", error)
+    })
+}
+
+function postData(body, url) {
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log(response)
+        return response.json()
+      } else {
+        throw Error(response.statusText)
+      }
+    })
+    .catch(error => {
+      console.log("Fetch error: ", error)
+    })
+}
+
+export { getData, postData, deleteData }  
--- src/app/App.js
+++ src/app/App.js
@@ -3,26 +3,36 @@ import React, { useEffect, useState } from 'react'
 // import { Counter } from '../features/counter/Counter';
 import './App.css'
 import RestaurantPreviewContainer from '../RestaurantPreviewContainer/RestaurantPreviewContainer'
-import { getRestaurants } from "../apiCalls"
+import { getData, getRestaurants, postData, deleteData } from "../apiCalls"
 import { Route, Routes } from 'react-router-dom'
 
+const URLMenuItems = "https://a1ecae6b-2320-4cd4-91ed-7da641c93480.mock.pstmn.io/api/v1/restaurants/:restaurant_id/menu_items"
+const URLRestaurants = "https://a1ecae6b-2320-4cd4-91ed-7da641c93480.mock.pstmn.io//api/v1/restaurants"
 
 const App = () => {
   const [restaurants, setRestaurants] = useState([])
+  const [menuItems, setMenuItems] = useState([])
 
   useEffect(() => {
-    getRestaurants().then(data => {
-      console.log("DATA", data.data)
+    getData(URLRestaurants).then(data => {
+      console.log('RESTAURANTS', data)
       setRestaurants(data.data)
     })
+    
+    getData(URLMenuItems).then(data => {
+      console.log("MENU ITEMS", data)
+      setMenuItems(data.data)
+    })
   }, [])
+
   return (
     <main className="App">
       <h1 className="app-title">Menu-ify</h1>
       <Routes>
         <Route path="/"
           element={<RestaurantPreviewContainer restaurants={restaurants} />}
-          />
+        />
+        {/* <Route path="/*" element={<pageNotFound />} /> */}
       </Routes>
     </main>
   )
