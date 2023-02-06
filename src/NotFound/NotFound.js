024397a9d90c2efba4e4d0dda48b8dabc826d32b
Justen Anderson <justenanderson@gmail.com>
2023-02-06 14:53:32 -0700

    Add error message and some styling.

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
   return (
     <div className="not-found">
-      Resource Not Found
+      <h1 className="nf-title">404</h1>
+      <p className="nf-instructions">So sorry! We can't find that restaurant.</p>
     </div>
 
   )
