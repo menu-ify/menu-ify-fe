8f4038d7e444e4bb23dce912039ce2feb0d3fe7f
Eleanor Ruth <ellie.ruth@me.com>
2023-02-05 18:36:20 -0700

    Create Menu component

---
+++ src/Menu/Menu.js
@@ -0,0 +1,54 @@
+import React, {useEffect, useState} from "react";
+import { useParams } from "react-router";
+import { getData } from "../apiCalls";
+import MenuItems from "../MenuItems/MenuItems";
+import './Menu.css'
+
+const Menu = () => {
+  const [menuItems, setMenuItems] = useState([])
+  const { id } = useParams();
+  //When we have the actauly server the end point will be 
+  //`http://localhost:3001/api/v1/restaurants/${id}/menu_items`
+  useEffect(()=> {
+    getData("https://a1ecae6b-2320-4cd4-91ed-7da641c93480.mock.pstmn.io/api/v1/restaurants/200/menu_items")
+      .then(data=>{
+        console.log(data.data)
+        setMenuItems(data.data)
+      })
+  }, [id])
+  const filterByCategory = (category) => {
+    console.log("CATEGORY", category)
+    console.log("MENUITEMS", menuItems)
+    return menuItems.filter(menuItem => menuItem.attributes.category === category)
+  }
+  const filteredMenuItems = (category) => filterByCategory(category).map(menuItems => {
+    return (
+      <MenuItems
+        key = {menuItems.id}
+        name = {menuItems.attributes.name}
+        image = {menuItems.attributes.image}
+        price = {menuItems.attributes.price}
+        description = {menuItems.attributes.description}
+      />
+
+    )
+  })
+  // const entreeMenuItems = filterByCategory("entree")
+  // const draftBeerMenuItems = filterByCategory("draft beer")
+  // const cocktailMenuItems = filterByCategory("cocktail")
+
+  return (
+    <section>
+      <h2>Appetizers</h2>
+      <section>{filteredMenuItems("appetizer")}</section>
+      <h2>Entree</h2>
+      <section>{filteredMenuItems("entree")}</section>
+      <h2>Draft Beer</h2>
+      <section>{filteredMenuItems("draft beer")}</section>
+      <h2>Cocktail</h2>
+      <section>{filteredMenuItems("cocktail")}</section>
+    </section>
+  )
+  }
+
+export default Menu
