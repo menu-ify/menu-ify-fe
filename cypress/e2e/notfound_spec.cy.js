2c2e15c3477d7b9500013c8f0a1f2a2d0bff9d36
Justen Anderson <justenanderson@gmail.com>
2023-02-07 14:54:53 -0700

    Add add'l tests.

--- cypress/e2e/notfound_spec.cy.js
+++ cypress/e2e/notfound_spec.cy.js
@@ -2,8 +2,12 @@ describe('404 Error page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/socks')
   })
-  it('should be visible on undesignated routes', () => {
-    expect(true).to.be.true
+  it('should only be visible on undesignated routes', () => {
+    cy.get('.nf-title').should('be.visible')
+    cy.visit('http://localhost:3000/')
+    cy.get('.rpc-title').should('be.visible')
+    cy.visit('http://localhost:3000/potatoes')
+    cy.get('.nf-title').should('be.visible')
   })
   it('should have a title', () => {
     cy.get('.name').contains('Menuify')
@@ -13,11 +17,19 @@ describe('404 Error page', () => {
     cy.get('.nf-instructions').contains('Page Not Found')
   })
   it('should have an image', () => {
-    cy.get('.error-image').should('have.attr', 'src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Sorry_we_are_closed_sign.jpg/640px-Sorry_we_are_closed_sign.jpg')
-    cy.get('.error-image').should('have.attr', 'alt', 'Sorry We Are Closed sign hanging in a restaurant window')
+    cy.get('.error-image').should(
+      'have.attr',
+      'src',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Sorry_we_are_closed_sign.jpg/640px-Sorry_we_are_closed_sign.jpg'
+    )
+    cy.get('.error-image').should(
+      'have.attr',
+      'alt',
+      'Sorry We Are Closed sign hanging in a restaurant window'
+    )
   })
   it('should have a link to the welcome page', () => {
     cy.get('.error-card-container').click()
-    cy.url().to.be('http://localhost:3000/')
+    cy.url().should('eq', 'http://localhost:3000/')
   })
 })
