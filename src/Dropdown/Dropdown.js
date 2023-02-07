1677acee3ca3fa9bf3d27f08aa96f7a901911453
Tim T <tim.email.personal@gmail.com>
2023-02-06 15:56:20 -1000

    Add styling to navbar and navlinks to hamburger menu

--- src/Dropdown/Dropdown.js
+++ src/Dropdown/Dropdown.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { NavLink } from "react-router-dom"
 import "./Dropdown.css"
 
 // This component will need access to the data storing all menus so it can update them dynamically.
@@ -6,41 +6,51 @@ import "./Dropdown.css"
 export default function Dropdown() {
   return (
     <ul className="dropdown">
-      <li className="dropdown__menu-item">
-        <button className="dropdown__button" onClick={() => {
-          console.log('Home clicked')
-        }}>
-          Home
-        </button>
-      </li>
-      <li className="dropdown__menu-item">
-        <button className="dropdown__button" onClick={() => {
-          console.log('Admin clicked')
-        }}>
-          Admin
-        </button>
-      </li>
-      <li className="dropdown__menu-item">
-        <button className="dropdown__button" onClick={() => {
-          console.log('Menu 1 clicked')
-        }}>
-          Menu 1
-        </button>
-      </li>
-      <li className="dropdown__menu-item">
-        <button className="dropdown__button" onClick={() => {
-          console.log('Menu 2 clicked')
-        }}>
-          Menu 2
-        </button>
-      </li>
-      <li className="dropdown__menu-item">
-        <button className="dropdown__button" onClick={() => {
-          console.log('Menu 3 clicked')
-        }}>
-          Menu 3
-        </button>
-      </li>
+      <NavLink to={`/`}>
+        <li className="dropdown__menu-item">
+          <button className="dropdown__button" onClick={() => {
+            console.log('Home clicked')
+          }}>
+            Home
+          </button>
+        </li>
+      </NavLink>
+      <NavLink to={`/admin`}>
+        <li className="dropdown__menu-item">
+          <button className="dropdown__button" onClick={() => {
+            console.log('Admin clicked')
+          }}>
+            Admin
+          </button>
+        </li>
+      </NavLink>
+      <NavLink to={`/restaurant/100`}>
+        <li className="dropdown__menu-item">
+          <button className="dropdown__button" onClick={() => {
+            console.log('Menu 1 clicked')
+          }}>
+            Menu 1
+          </button>
+        </li>
+      </NavLink>
+      <NavLink to={`/restaurant/200`}>
+        <li className="dropdown__menu-item">
+          <button className="dropdown__button" onClick={() => {
+            console.log('Menu 2 clicked')
+          }}>
+            Menu 2
+          </button>
+        </li>
+      </NavLink>
+      <NavLink to={`/restaurant/300`}>
+        <li className="dropdown__menu-item">
+          <button className="dropdown__button" onClick={() => {
+            console.log('Menu 3 clicked')
+          }}>
+            Menu 3
+          </button>
+        </li>
+      </NavLink>
     </ul>
   )
 }
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -6,26 +6,32 @@ export default function NavBar() {
   const [menuStatus, setMenuStatus] = useState(false)
   // maybe this should use our Redux store to track the menu's open/close state, navlinks, etc since navbar is visible on all app pages
 
-  const restaurantName = "Testaurant Name"
+  const restaurantName = "Menuify"
 
   return (
     <>
       <div className="frame">
-        <header className="name">{restaurantName}</header>
-        <button className="menu" onClick={() => {
-          console.log('click')
-          setMenuStatus(!menuStatus)
-        }}>
-          {!menuStatus && <span className="material-symbols-outlined icon">
-            menu
-          </span>
-          }
-          {menuStatus && <span className="material-symbols-outlined icon">
-            close
-          </span>
-          }
-        </button>
+        <div className="button-container">
+          <button className="menu" onClick={() => {
+            console.log('click')
+            setMenuStatus(!menuStatus)
+          }}>
+            {!menuStatus && <span className="material-symbols-outlined icon">
+              menu
+            </span>
+            }
+            {menuStatus && <span className="material-symbols-outlined icon">
+              close
+            </span>
+            }
+          </button>
+        </div>
         {menuStatus && <Dropdown />}
+
+        <div className="nav-title-container">
+          <header className="name">{restaurantName}</header>
+        </div>
+
       </div>
     </>
   )
