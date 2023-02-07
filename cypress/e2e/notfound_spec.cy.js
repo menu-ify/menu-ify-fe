5592a346902665d3c4f37916355a3da343702949
Justen Anderson <justenanderson@gmail.com>
2023-02-07 14:44:50 -0700

    Add spec file and initial tests for the NotFound component.

---
+++ cypress/e2e/notfound_spec.cy.js
@@ -0,0 +1,23 @@
+describe('404 Error page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/socks')
+  })
+  it('should be visible on undesignated routes', () => {
+    expect(true).to.be.true
+  })
+  it('should have a title', () => {
+    cy.get('.name').contains('Menuify')
+  })
+  it('should have an error message', () => {
+    cy.get('.nf-title').contains('404')
+    cy.get('.nf-instructions').contains('Page Not Found')
+  })
+  it('should have an image', () => {
+    cy.get('.error-image').should('have.attr', 'src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Sorry_we_are_closed_sign.jpg/640px-Sorry_we_are_closed_sign.jpg')
+    cy.get('.error-image').should('have.attr', 'alt', 'Sorry We Are Closed sign hanging in a restaurant window')
+  })
+  it('should have a link to the welcome page', () => {
+    cy.get('.error-card-container').click()
+    cy.url().to.be('http://localhost:3000/')
+  })
+})
