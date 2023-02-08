6108174f6497070bb0398b3b3948fa0f2bad9d3d
James Wasmer <jameswasmer@gmail.com>
2023-02-08 18:02:28 -0500

    Remove outdated comment

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -4,7 +4,6 @@ import Dropdown from "../Dropdown/Dropdown"
 
 export default function NavBar() {
   const [menuStatus, setMenuStatus] = useState(false)
-  // maybe this should use our Redux store to track the menu's open/close state, navlinks, etc since navbar is visible on all app pages
 
   const restaurantName = "Menuify"
 
