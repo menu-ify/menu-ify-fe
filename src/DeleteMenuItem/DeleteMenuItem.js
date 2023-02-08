209d9cd2a15be3b699a7df0717d894a1b1f7adb8
Tim T <tim.email.personal@gmail.com>
2023-02-08 08:02:03 -1000

    Remove console log to pass circleCi

--- src/DeleteMenuItem/DeleteMenuItem.js
+++ src/DeleteMenuItem/DeleteMenuItem.js
@@ -2,7 +2,6 @@ import React from "react"
 
 const DeleteMenuItem = ({ adminSelections }) => {
   console.log(adminSelections)
-  {console.log("ADMIN SELECTIONS",adminSelections)}
   return (
     <div>
     <h1>Delete View</h1>
