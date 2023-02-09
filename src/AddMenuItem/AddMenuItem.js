ac852a064c8d661f056e3f4ac3143efe3798efea
James Wasmer <jameswasmer@gmail.com>
2023-02-08 19:43:55 -0500

    Build placeholder API call for image search

--- src/AddMenuItem/AddMenuItem.js
+++ src/AddMenuItem/AddMenuItem.js
@@ -1,18 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import "./AddMenuItem.css"
 import MenuItems from '../MenuItems/MenuItems'
+import { getData } from '../apiCalls'
 
 export default function AddMenuItem() {
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
   const [price, setPrice] = useState('')
   const [search, setSearch] = useState('')
+  const [images, setImages] = useState(null)
   const [image, setImage] = useState('')
+
+  useEffect(() => {
+    if (images) return;
+
+    const searchableImages = getData('https://menu-ify-be.herokuapp.com/api/v1/restaurants')
+    .then(data => {
+      setImages(data)
+    })
+  })
+
+  const searchImages = () => {
+
+  }
 
   return (
     <div className="add-item-container">
       <header>
-        Add to your menu:
+        Build a new menu item:
       </header>
       <form className="form">
         <input name="name" type="text" placeholder="Enter name..." value={name} onChange={(e) => {
@@ -31,7 +46,7 @@ export default function AddMenuItem() {
           <button>Search</button>
         </div>
         <div className="search-results">
-          
+          {getSearchResults(search, searchableImages) && }
         </div>
         <MenuItems name={name} description={description} image={"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Pictograms-nps-food_service.svg/640px-Pictograms-nps-food_service.svg.png"} price={price}/>
       </form>
