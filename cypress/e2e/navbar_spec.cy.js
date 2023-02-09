73aa0149e47fe721d61be0a77ec7b77c64bcc557
Eleanor Ruth <ellie.ruth@me.com>
2023-02-09 07:13:05 -0700

    Update cypress test to account for changes to the nav bar

--- cypress/e2e/navbar_spec.cy.js
+++ cypress/e2e/navbar_spec.cy.js
@@ -6,7 +6,7 @@ describe('The NavBar component', () => {
     cy.get('.name1').contains('Menu')
     cy.get('.name2').contains('ify')
     cy.get('.material-symbols-outlined').click()
-    cy.get('[href="/"] > .dropdown__menu-item > .dropdown__button').contains('Home')
+    cy.get('[href="/"] > .dropdown__menu-item > .dropdown__button').contains('All restaurants')
     cy.get('[href="/admin"] > .dropdown__menu-item > .dropdown__button').contains('Admin')
     cy.get('[href="/restaurant/100"] > .dropdown__menu-item > .dropdown__button').contains('Pho Kyah')
     cy.get('[href="/restaurant/200"] > .dropdown__menu-item > .dropdown__button').contains("Tim's Tiki Bar")
