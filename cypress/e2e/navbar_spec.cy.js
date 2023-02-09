f0e96dd747653f6fa0ca96a4a993e2e3e09cfd68
Eleanor Ruth <ellie.ruth@me.com>
2023-02-09 06:43:52 -0700

    change test url's to intercept the real api

--- cypress/e2e/navbar_spec.cy.js
+++ cypress/e2e/navbar_spec.cy.js
@@ -1,6 +1,6 @@
 describe('The NavBar component', () => {
   it('should have a header with a title and dropdown menu', () => {
-    const url = 'https://a1ecae6b-2320-4cd4-91ed-7da641c93480.mock.pstmn.io//api/v1/restaurants'
+    const url = 'https://menu-ify-be.herokuapp.com/api/v1/restaurants'
     cy.intercept(url, { fixture: '../fixtures/restaurant_data.json' })
     cy.visit('http://localhost:3000/')
     cy.get('.name1').contains('Menu')
--- cypress/e2e/restaurantpreviewcontainer_spec.cy.js
+++ cypress/e2e/restaurantpreviewcontainer_spec.cy.js
@@ -1,6 +1,6 @@
 describe('RestaurantPreviewContainer', () => {
   beforeEach(() => {
-    const url = 'https://a1ecae6b-2320-4cd4-91ed-7da641c93480.mock.pstmn.io//api/v1/restaurants'
+    const url = 'https://menu-ify-be.herokuapp.com/api/v1/restaurants'
     cy.intercept(url, { fixture: '../fixtures/restaurant_data.json' })
     cy.visit('http://localhost:3000/')
   })
