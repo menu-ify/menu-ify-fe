fb77ebf57ba93c5cf805d78cbf811241dd0a6ef2
Justen Anderson <justenanderson@gmail.com>
2023-02-10 09:46:00 -0600

    Re-stubbed all network requests after merge issue.

--- cypress/e2e/notfound_spec.cy.js
+++ cypress/e2e/notfound_spec.cy.js
@@ -1,5 +1,6 @@
 describe('404 Error page', () => {
   beforeEach(() => {
+    cy.intercept('https://menu-ify-be.herokuapp.com/api/v1/restaurants', { fixture: '../fixtures/restaurant_data.json' })
     cy.visit('http://localhost:3000/socks')
   })
 
@@ -9,14 +10,24 @@ describe('404 Error page', () => {
     cy.get('.rpc-title').should('be.visible')
     cy.visit('http://localhost:3000/potatoes')
     cy.get('.nf-title').should('be.visible')
+
+    cy.intercept('https://menu-ify-be.herokuapp.com/api/v1/restaurants/100/menu_items', { fixture: '../fixtures/pho_kyah_menu.json' })
     cy.visit('http://localhost:3000/restaurant/100')
     cy.get(':nth-child(1) > .category-title').contains('Appetizers')
+
+    cy.intercept('https://menu-ify-be.herokuapp.com/api/v1/restaurants/200/menu_items', { fixture: '../fixtures/tims_menu.json' })
     cy.visit('http://localhost:3000/restaurant/200')
     cy.get(':nth-child(2) > .category-title').contains('Entrees')
+
+    cy.intercept('https://menu-ify-be.herokuapp.com/api/v1/restaurants/300/menu_items', { fixture: '../fixtures/ruthys_menu.json' })
     cy.visit('http://localhost:3000/restaurant/300')
     cy.get(':nth-child(3) > .category-title').contains('Draft Beer')
+
+    cy.intercept('https://menu-ify-be.herokuapp.com/api/v1/restaurants/400/menu_items', { fixture: '../fixtures/pho_kyah_menu.json' })
     cy.visit('http://localhost:3000/restaurant/400')
     cy.get('.NFC-title').contains('Explore more menus')
+
+    cy.intercept('https://menu-ify-be.herokuapp.com/api/v1/restaurants/socks/menu_items', { fixture: '../fixtures/pho_kyah_menu.json' })
     cy.visit('http://localhost:3000/restaurant/socks')
     cy.get('.NFC-description').contains('Tap or click for other restaurants')
     const url = '/undesignated-route'
