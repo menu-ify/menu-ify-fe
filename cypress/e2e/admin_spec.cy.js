ef1e55578dfd600800fd032641ecc4bb2e9bc1c5
Justen Anderson <justenanderson@gmail.com>
2023-02-11 10:10:01 -0600

    Add drop down menu tests.

--- cypress/e2e/admin_spec.cy.js
+++ cypress/e2e/admin_spec.cy.js
@@ -8,7 +8,15 @@ describe('The Admin page', () => {
   it('should have a page title and instructions', () => {
     cy.get('.rpc-title').contains('Admin View')
     cy.get('.admin-instructions-container > :nth-child(2)').contains('Complete the following')
+    cy.get('.form-instructions > :nth-child(1)').contains('1. Select a restaurant to edit')
+    cy.get('.form-instructions > :nth-child(3)').contains('2. Select the action to take')
+    cy.get('.form-instructions > :nth-child(5)').contains('3. When all fields are completed click "Get started".')
   })
 
-
+  it('should have a drop down menu to select a restaurant', () => {
+    cy.get(':nth-child(2) > .form-select').contains('None selected')
+    cy.get(':nth-child(2) > .form-select').contains('Pho Kyah')
+  })
+  it('should have a drop down menu to select an action to take', () => {})
+  it('should have a drop down menu to select a restaurant', () => {})
 })
