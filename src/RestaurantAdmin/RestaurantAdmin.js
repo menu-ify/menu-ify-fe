0cbbe23f0c71743ee2690131ddb2a1ab3db78010
Tim T <tim.email.personal@gmail.com>
2023-02-12 11:19:08 -1000

    Remove use effect to pass circle ci

--- src/RestaurantAdmin/RestaurantAdmin.js
+++ src/RestaurantAdmin/RestaurantAdmin.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import "../RestaurantAdmin/RestaurantAdmin.css"
 import { postData, deleteData, getData } from "../apiCalls"
 
