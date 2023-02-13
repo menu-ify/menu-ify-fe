3df28d5811c07f84fdb448d9b3c5983d3f72792f
James Wasmer <jameswasmer@gmail.com>
2023-02-13 18:22:11 -0500

    Remove unused useEffect import

--- src/AddMenuItem/AddMenuItem.js
+++ src/AddMenuItem/AddMenuItem.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import "./AddMenuItem.css"
 import MenuItems from '../MenuItems/MenuItems'
 import { getData } from '../apiCalls'
