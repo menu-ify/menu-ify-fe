c13fa6ed0427c62856ab412669e0ea46f7e591fe
Justen Anderson <justenanderson@gmail.com>
2023-02-14 15:35:21 -0600

    Add tests for the delete-restaurant section.

--- cypress/e2e/adddeleterestaurant_spec.cy.js
+++ cypress/e2e/adddeleterestaurant_spec.cy.js
@@ -6,9 +6,12 @@ describe('The add/delete restaurant user flow', () => {
     cy.visit('http://localhost:3000/admin/restaurant')
   })
   
-  it('should have a title', () => {
+  it('should have a title and instructions', () => {
     cy.get(':nth-child(2) > .rpc-title').contains('Admin View')
     cy.get(':nth-child(2) > .rpc-instructions').contains('Add a restaurant')
+    cy.get('.delete-margin > .rpc-title').contains('Admin View')
+    cy.get('.delete-margin > .rpc-instructions').contains('Delete a restaurant')
+    cy.get('.search-button').contains('Add new restaurant')
   })
 
   it('should have a preview of the restaurant to be added', () => {
@@ -20,6 +23,21 @@ describe('The add/delete restaurant user flow', () => {
     cy.get('.RPC-description').contains('test description')
   })
 
-  
+  it.only('should show the current restaurants and have buttons to delete them', () => {
+    cy.get(':nth-child(3) > .delete-header').contains('Pho Kyah')
+    cy.get('.delete-margin > :nth-child(3)').contains('Delete')
+    cy.get(':nth-child(4) > .delete-header').contains('Tim\'s Tiki Bar')
+    cy.get('.delete-margin > :nth-child(4)').contains('Delete')
+    cy.get(':nth-child(5) > .delete-header').contains('Ruthy\'s')
+    cy.get('.delete-margin > :nth-child(5)').contains('Delete')
+  })
+
+  it('should add a new restaurant to the home page', () => {
+    cy.get('[placeholder="Enter name..."]').type('test title')
+    cy.get('[placeholder="Enter description..."]').type('test description')
+    cy.get('[placeholder="Enter URL to image..."]').type('https://s3.memeshappen.com/memes/Testing-.jpg')
+    cy.get('.search-button').click()
+    // cy.intercept('POST', 'https://menu-ify-be.herokuapp.com/api/v1/restaurants/', { fixture: '../fixtures/add_restaurant_data.json' })
+  })
 
 })
