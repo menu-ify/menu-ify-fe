d341d2a48b5c7220ea8a7442a23772ebf404dac4
Eleanor Ruth <ellie.ruth@me.com>
2023-02-14 17:05:33 -0700

    fix Cypress test

--- cypress/e2e/adddeleterestaurant_spec.cy.js
+++ cypress/e2e/adddeleterestaurant_spec.cy.js
@@ -17,7 +17,7 @@ describe('The add/delete restaurant user flow', () => {
   it('should have a preview of the restaurant to be added', () => {
     cy.get('[placeholder="Enter name..."]').type('test title')
     cy.get('[placeholder="Enter description..."]').type('test description')
-    cy.get('[placeholder="Enter URL to image..."]').type('https://s3.memeshappen.com/memes/Testing-.jpg')
+    cy.get('[placeholder="Search for image..."]').type('https://s3.memeshappen.com/memes/Testing-.jpg')
     cy.get('.restaurant-image').should('have.attr', 'src', 'https://s3.memeshappen.com/memes/Testing-.jpg')
     cy.get('.RPC-title').contains('test title')
     cy.get('.RPC-description').contains('test description')
